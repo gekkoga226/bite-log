a51308a00a783668fd18a71a8dda0f4a951bec0a
mizuko226 <mizuko226@gmail.com>
2026-06-14 01:47:47 +0900

    feat: add Goals interface to goals.ts

--- src/lib/goals.ts
+++ src/lib/goals.ts
@@ -1,4 +1,11 @@
-export const DEFAULT_GOALS = {
+export interface Goals {
+  calories: number
+  protein: number
+  fat: number
+  carbs: number
+}
+
+export const DEFAULT_GOALS: Goals = {
   calories: 1800,
   protein: 93,
   fat: 42,
